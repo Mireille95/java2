5a5e4bf73c8ce2f1e69933d5ef39567f9be312a3
mireille <mimimireille95@gmail.com>
2019-07-23 06:54:39 -0400

    jqueries,dom,form,calculato

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
-jQuery(document).ready(function() {
-jQuery("h1").click(function() {
-    alert("This is a header.");
-    alert("I told you, THIS IS A HEADER!");
-    });
+// jQuery(document).ready(function() {
+// jQuery("h1").click(function() {
+//     alert("This is a header.");
+//     alert("I told you, THIS IS A HEADER!");
+//     });
   
-    jQuery("p").click(function() {
-      alert("This is a paragraph.");
-    });
+//     jQuery("p").click(function() {
+//       alert("This is a paragraph.");
+//     });
   
-    jQuery("img").click(function() {
-      alert("This is an image.");
-    });
-  });
+//     jQuery("img").click(function() {
+//       alert("This is an image.");
+//     });
+//   });
 
 
 
@@ -145,30 +145,51 @@ jQuery("h1").click(function() {
 //   });
 // });
 
-var add = function(number1, number2) {
-  return number1 + number2;
-  };
-  var subtract = function(number1, number2) {
-  return number1 - number2;
-  };
-  var multiply = function(number1, number2) {
-  return number1 * number2;
-  };
-  var divide = function(number1, number2) {
-  return number1 / number2;
-  };
+/////////////////////////////////calculator////////////////////////////
+// var add = function(number1, number2) {
+//   return number1 + number2;
+//   };
+//   var subtract = function(number1, number2) {
+//   return number1 - number2;
+//   };
+//   var multiply = function(number1, number2) {
+//   return number1 * number2;
+//   };
+//   var divide = function(number1, number2) {
+//   return number1 / number2;
+//   };
   
-  $(document).ready(function() {
-  $("form#add").submit(function(event) {
-  event.preventDefault();
-  var number1 = parseInt($("#add1").val());
-  var number2 = parseInt($("#add2").val());
-  var result = add(number1, number2);
-  $("#output").text(result);
-  });
-  });
+//   $(document).ready(function() {
+//   $("form#add").submit(function(event) {
+//   event.preventDefault();
+//   var number1 = parseInt($("#add1").val());
+//   var number2 = parseInt($("#add2").val());
+//   var result = add(number1, number2);
+//   $("#output").text(result);
+//   });
+//   });
+
+//////////////////////form with button, radion,date/////////////////////////////
 // var beverage = $("#beverage").val();
 // var flavor = $("input:radio[name=flavor]:checked").val();
 // var dob = $("#born").val();
 // var favoriteColor = $("#color").val();
- 
+
+
+ //////////////////////////////////form fill//////////////////////////
+//  $(document).ready(function() {
+//         $("#blanks form").submit(function(event) {
+//           var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+
+//           blanks.forEach(function(blank) {
+//             var userInput = $("input." + blank).val();
+//             console.log(userInput);
+//             $("." + blank).text(userInput).val();
+//           });
+
+//           $("#story").show();
+
+//           event.preventDefault();
+//         });
+//       });
+    
