0f77bff738c3892b2dbac192b612be4f97db111e
mireille <mimimireille95@gmail.com>
2019-07-23 05:32:07 -0400

    jquery,DOm,

---
+++ js/index.js
@@ -0,0 +1,84 @@
+// jQuery(document).ready(function() {
+// jQuery("h1").click(function() {
+//     alert("This is a header.");
+//     alert("I told you, THIS IS A HEADER!");
+//     });
+  
+//     jQuery("p").click(function() {
+//       alert("This is a paragraph.");
+//     });
+  
+//     jQuery("img").click(function() {
+//       alert("This is an image.");
+//     });
+//   });
+
+
+
+// img{
+//   display: none;
+// }
+// .walrus-showing {
+//   display: none;
+// }
+
+// .clickable {
+//   cursor: pointer;
+//   color: #0088cc;
+// }
+
+// .clickable:hover {
+//   text-decoration: underline;
+// }
+
+// $(document).ready(function() {
+//   $("button#green").click(function() {
+//     $("body").addClass("green-background");
+//   });
+
+//   $("button#yellow").click(function() {
+//     $("body").addClass("yellow-background");
+//   });
+
+//   $("button#red").click(function() {
+//     $("body").addClass("red-background");
+//   });
+// });
+// $(document).ready(function() {
+//   $("button#green").click(function() {
+//     $("body").removeClass();
+//     $("body").addClass("green-background");
+//   });
+
+//   $("button#yellow").click(function() {
+//     $("body").removeClass();
+//     $("body").addClass("yellow-background");
+//   });
+
+//   $("button#red").click(function() {
+//     $("body").removeClass();
+//     $("body").addClass("red-background");
+//   });
+// });
+
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+  // $("button#green").click(function() {
+  //   $("body").css("background-color", "green");
+  // });
+});
+
+
